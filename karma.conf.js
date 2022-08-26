5e4352f9ac6d9fce8558177c09622229be5556df
Austen Stone <austenstone@github.com>
2022-08-26 05:47:11 -0400

    test

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-codespace'),
+      dir: require('path').join(__dirname, './coverage/reddit-youtube'),
       subdir: '.',
       reporters: [
         { type: 'html' },
